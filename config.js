2a624248a85b226bbd3bc34e12a9a9b84f90a003
Human1234h <152832850+Human1234h@users.noreply.github.com>
2024-09-27 10:08:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "NKE1AaYZ#4hdRqH603kdBykmZ-JjLRypbE9fUwlvJfAognz_NGcI",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
